afd0954ce56afff1e1e9d45e5da67c1b664d8fda
anirudhkrishna28 <anirudhkrishna.s2021csea@sece.ac.in>
2023-07-08 15:12:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Auth } from './pages/Auth'
+import { CreateRecipe } from './pages/CreateRecipe'
+import { SavedRecipes }  from './pages/SavedRecipes'
+import { NavBar } from './components/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <Router>
+          <NavBar/>
+          <Routes>
+          <Route path='/' element = {<Home/>} />
+          <Route path='/auth' element = {<Auth/>} />
+          <Route path='/create-recipe' element = {<CreateRecipe/>} />
+          <Route path='/saved-recipes' element = {<SavedRecipes/>} />
+          </Routes>
+        </Router>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
